3ff32f7e52c92bcad0457dd3a4bb1603f3f97d96
Carl Wilson <carl@openplanetsfoundation.org>
2014-02-04 09:39:05 +0000

    Commented bootstrap.sh provisioning script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
 
+##
+# Bash script to provision VM, used to set up test environment.
+# The is the correct home for one time builds/installations 
+# required to set up the demonstrators.
+#
+# Be aware this script is only the first time you issue the
+#    vagrant up
+# command, or following a
+#    vagrant destroy
+#    vagrant up
+# combination.  See the README for further detais.
+##
+
+# Update apt repos
 apt-get update
+
+# Install apache 2 and PHP 5
 apt-get install -y apache2
 apt-get install -y php5 libapache2-mod-php5
-apt-get install -y git make cmake
-apt-get install -y libfftw3-dev libboost-all-dev
 
+# Restart apache and link www root to the vagrant shared dir
+# which is the project home directroy. This allows live edits
+# on the host machine to be immediately available of the VM.
 /etc/init.d/apache2 restart
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
+# Install tools for downloading and building xcorrsound 
+apt-get install -y git make cmake
+apt-get install -y libfftw3-dev libboost-all-dev
+
+# Build xcorrsound package in temp
 cd /tmp
 git clone https://github.com/openplanets/scape-xcorrsound.git
 cd scape-xcorrsound
@@ -17,4 +39,7 @@ mkdir build && cd build
 cmake ..
 make package
 cpack -G DEB
+
+# Install xcorrsound package
 dpkg -i scape-xcorrsound*deb
+
