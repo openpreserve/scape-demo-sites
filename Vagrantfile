fff093993da54a1428ea29d4c9836443d58d28f4
carlwilson <carl@openplanetsfoundation.org>
2014-02-14 12:00:40 +0000

    Vagrant provisioning in explicit directory.

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,13 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+    # Use the precised64 box, change this for 32 bit, or other distro
     config.vm.box = "precise64"
-    config.vm.provision :shell, :path => "bootstrap.sh"
+
+    # Run the provisioning script	
+    config.vm.provision :shell, :path => "./provision/bootstrap.sh"
+
+    # Port forward HTTP (80) to host 2020
     config.vm.network :forwarded_port, host: 2020, guest: 80
 end
--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -13,14 +13,17 @@
 # combination.  See the README for further detais.
 ##
 
-# Update apt repos
+# Add openplanets Bintray deb repo and udate apt repos
+echo "deb http://dl.bintray.com/openplanets/opf-debian /" >> /etc/apt/sources.list 
 apt-get update
 
 # Install apache 2 and PHP 5 for demo site
 apt-get install -y apache2 php5 libapache2-mod-php5
 
-# Install firefox, xvfb, and Java for pagelyzer 
-apt-get install -y firefox xvfb openjdk-7-jre-headless
+# Install firefox and Java for pagelyzer 
+apt-get install -y firefox openjdk-7-jre-headless
+#  Install fonts and xvfb for for virtual X window 
+apt-get install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic xvfb x11-apps  imagemagick
 
 # Restart apache and link www root to the vagrant shared dir
 # which is the project home directroy. This allows live edits
@@ -46,28 +49,31 @@ cpack -G DEB
 # Install xcorrsound package
 dpkg -i scape-xcorrsound*deb
 
+# Download and install jpylyzer package
+apt-get install -y jpylyzer
+
 # Download Selenium to /var/lib/selenium
 mkdir -p /var/lib/selenium
 wget -q -O /var/lib/selenium/selenium-server-standalone-2.39.0.jar http://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar 
 
 # Copy the Pagelyzer Jar to /var/lib/pagelyzer
 mkdir -p /var/lib/pagelyzer
-cp /vagrant/pagelyzer/jPagelyzer.jar /var/lib/pagelyzer
+cp /vagrant/lib/jPagelyzer.jar /var/lib/pagelyzer
 
 # Copy the Selenium Xvfb script to init.d and register it
-cp /vagrant/xvfb-sel /etc/init.d/xvfb-sel
+cp /vagrant/provision/services/xvfb-sel /etc/init.d/xvfb-sel
 chmod +x /etc/init.d/xvfb-sel
 update-rc.d xvfb-sel defaults
 service xvfb-sel start
 
 # Copy the pagelyzer Xvfb script to init.d and register it
-cp /vagrant/xvfb-pag /etc/init.d/xvfb-pag
+cp /vagrant/provision/services/xvfb-pag /etc/init.d/xvfb-pag
 chmod +x /etc/init.d/xvfb-pag
 update-rc.d xvfb-pag defaults
 service xvfb-pag start
 
 # Copy the selenium script to init.d and register it
-cp /vagrant/selenium /etc/init.d/selenium
+cp /vagrant/provision/services/selenium /etc/init.d/selenium
 chmod +x /etc/init.d/selenium
 update-rc.d selenium defaults
 service selenium start
