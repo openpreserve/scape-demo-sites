75492b3e4361826de1a21b544fc5087a74c9139a
Roman <Graf>
2014-04-01 23:22:28 +0200

    + new version of matchbox demo presentation.

--- matchbox/check.php
+++ matchbox/check.php
@@ -1,31 +1,10 @@
  <?php 
 
-
-
-require_once "../StyleJS/json/JSON.php";
-$json = new Services_JSON();
-
-$url = $_GET["url"];
-
-$descriptorspec = array(
-    0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-    1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-    2 => array("pipe", "w")   // 2 is STDERR for process
- );
-
-$cmd = "DISPLAY=:0 python2.7 ./FindDuplicates.py ".$url." all";
-
-$p=proc_open ($cmd, $descriptorspec, $pipes);
-
-$out = stream_get_contents($pipes[1]) ;
-
-$out2 = $json->encode($out);
-$parts = explode('\n', $out2);
-
-$result = $json->encode($parts[sizeof($parts)-1]);
-print($result);
-
-proc_close($p);
-
+	//expecting Matchbox is set up and configured
+ 	$output = array();
+  	$command = "python2.7 ./FindDuplicates.py " . escapeshellarg($_FILES['matchboxToValidate']["tmp_name"]) . " all";
+	print($command);
+   	exec($command, $output);
+	print($output);
 
 ?>
